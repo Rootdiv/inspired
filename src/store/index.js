6d13913af41ca13d5b1133594f7166e6ba81d19e
Rootdiv <rootdiv@mail.ru>
2023-07-06 22:17:15 +0300

    fix: added Redux Toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import { rootReducer } from './rootReducer';
 
 const store = configureStore({
   reducer: rootReducer,
-  devTools: import.meta.DEV,
+  devTools: import.meta.env.DEV,
 });
 
 export default store;
