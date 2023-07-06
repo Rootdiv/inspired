2e8daddff1189c655bc922b4346d6d8c3e92fb4b
Rootdiv <rootdiv@mail.ru>
2023-07-06 12:44:45 +0300

    feat: added Copyright component

---
+++ src/Components/Footer/Copyright/Copyright.jsx
@@ -0,0 +1,7 @@
+import style from './Copyright.module.scss';
+
+export const Copyright = () => (
+  <div className={style.copyright}>
+    <p>&copy; INSPIRED, 2023</p>
+  </div>
+);
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,9 @@
 import { Container } from '@/Components/Layout/Container/Container';
 import { Category } from '@/Components/Footer/Category/Category';
 import { Social } from '@/Components/Footer/Social/Social';
-import { Development } from '@/Components/Footer/Development/Development';
 import { Contacts } from '@/Components/Footer/Contacts/Contacts';
+import { Copyright } from '@/Components/Footer/Copyright/Copyright';
+import { Development } from '@/Components/Footer/Development/Development';
 
 import style from './Footer.module.scss';
 
@@ -13,9 +14,7 @@ export const Footer = () => (
         <Category />
         <Social />
         <Contacts />
-        <div className={style.copyright}>
-          <p>&copy; INSPIRED, 2023</p>
-        </div>
+        <Copyright />
         <Development />
       </div>
     </Container>
