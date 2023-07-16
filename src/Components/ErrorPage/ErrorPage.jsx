7ce08251ac7c0359aa5b47f871d69cc1aa305309
Rootdiv <rootdiv@mail.ru>
2023-07-17 00:18:05 +0300

    feat: added check status the server

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,44 @@
-import { useRouteError } from 'react-router-dom';
+import { useRouteError, useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import style from './ErrorPage.module.scss';
+import { useEffect, useRef } from 'react';
+import { fetchColors } from '@/features/colorsSlice';
+import { fetchNavigation } from '@/features/navigationSlice';
 
 export const ErrorPage = () => {
-  const error = useRouteError();
+  const routeError = useRouteError();
+  const { status } = useSelector(state => state.statusServer);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const timerIdRef = useRef(null);
+
+  useEffect(() => {
+    if (status && location.pathname === '/404') {
+      navigate('/');
+    }
+  }, [status, location.pathname, navigate]);
+
+  useEffect(() => {
+    if (!status && location.pathname === '/404') {
+      clearInterval(timerIdRef.current);
+
+      timerIdRef.current = setInterval(() => {
+        dispatch(fetchColors());
+        dispatch(fetchNavigation());
+      }, 3000);
+    }
+
+    return () => {
+      clearInterval(timerIdRef.current);
+    };
+  }, [status, location.pathname, dispatch]);
+
   return (
-    <div>
-      <h2>Ошибка 404</h2>
-      <p>{error?.message || 'Неизвестная ошибка'}</p>
+    <div className={style.error}>
+      <h2 className={style.title}>Произошла ошибка попробуйте зайти позже</h2>
+      <p className={style.message}>{routeError?.message || 'Неизвестная ошибка'}</p>
     </div>
   );
 };
--- src/Components/Layout/Main/Main.jsx
+++ src/Components/Layout/Main/Main.jsx
@@ -1,3 +1,18 @@
+import { useSelector } from 'react-redux';
 import style from './Main.module.scss';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-export const Main = ({ children }) => <main className={style.main}>{children}</main>;
+export const Main = ({ children }) => {
+  const { status } = useSelector(state => state.statusServer);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!status && location.pathname !== '/404') {
+      navigate('/404');
+    }
+  }, [status, location.pathname, navigate]);
+
+  return <main className={style.main}>{children}</main>;
+};
