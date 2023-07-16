dc5a40f9b41a5333b1739fd529ce9498cddd4c4c
Rootdiv <rootdiv@mail.ru>
2023-07-16 08:38:14 +0300

    fix: fixed counter

--- src/Components/Goods/Goods.jsx
+++ src/Components/Goods/Goods.jsx
@@ -8,10 +8,12 @@ export const Goods = ({ title }) => {
   const { goodsList } = useSelector(state => state.goods);
   const { totalCount } = useSelector(state => state.goods);
 
+  const counter = totalCount ? ` (${totalCount})` : '';
+
   return (
     <section className={style.goods}>
       <Container>
-        <h2 className={style.title}>{title ? `${title} (${totalCount})` : 'Новинки'}</h2>
+        <h2 className={style.title}>{title ? `${title}${counter}` : 'Новинки'}</h2>
         <ul className={style.list}>
           {goodsList.map(item => (
             <li key={item.id}>
