b2a4fe3b8b5f4a71f8ce0847ca6c3b89c1ddff22
Rootdiv <rootdiv9152@gmail.com>
2024-03-22 22:07:55 +0300

    feat: changed api server

--- src/const.js
+++ src/const.js
@@ -1,4 +1,5 @@
-export const API_URL = `${window.location.protocol}//${window.location.hostname}:8024`;
+export const API_URL = 
+  import.meta.env.DEV ? 'http://localhost:8024' : 'https://api.rootdiv.ru/inspired';
 export const GOODS_URL = `${API_URL}/api/goods`;
 export const CATEGORY_URL = `${API_URL}/api/categories`;
 export const COLORS_URL = `${API_URL}/api/colors`;
